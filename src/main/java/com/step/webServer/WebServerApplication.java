c73491d91ecbe1a0d57d67763cc679a854ac5a94
DovahYol <DovahYol@live.com>
2019-03-17 23:22:07 +0800

    Server Ready Branch

--- src/main/java/com/step/webServer/WebServerApplication.java
+++ src/main/java/com/step/webServer/WebServerApplication.java
@@ -2,6 +2,7 @@ package com.step.webServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,7 +13,7 @@ import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableAsync
-public class WebServerApplication extends WebMvcConfigurationSupport {
+public class WebServerApplication extends SpringBootServletInitializer {
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
